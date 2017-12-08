90bec6b8f16a937d28de42fa7bd0ddd42aa55693
Edward Viaene <ward.viaene@gmail.com>
2017-12-08 13:12:53 +0000

    validation update and unit test

---
+++ api_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDeployServiceValidator(t *testing.T) {
+	// api object
+	a := API{}
+
+	// test with 2 characters
+	d := Deploy{
+		Containers: []*DeployContainer{
+			{
+				ContainerName: "abc",
+			},
+		},
+	}
+	serviceName := "ab"
+	err := a.deployServiceValidator(serviceName, d)
+	if err == nil || err.Error() != "service name needs to be at least 3 characters" {
+		t.Errorf("Servicename with 2 characters didn't get error message")
+	}
+
+	// test with 3 characters
+	d = Deploy{
+		Containers: []*DeployContainer{
+			{
+				ContainerName: "abc",
+			},
+		},
+	}
+	serviceName = "abc"
+	err = a.deployServiceValidator(serviceName, d)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	// test with wrong container name
+	serviceName = "myservice"
+	d = Deploy{
+		Containers: []*DeployContainer{
+			{
+				ContainerName: "ab",
+			},
+			{
+				ContainerName: "abd",
+			},
+		},
+	}
+	serviceName = "abc"
+	err = a.deployServiceValidator(serviceName, d)
+	if err == nil {
+		t.Errorf("No containerName is equal to serviceName, but no error raised")
+	}
+}
