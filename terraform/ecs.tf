5d96b00dcf4e684e13aa8949541149b97097655a
Edward Viaene <ward.viaene@gmail.com>
2019-01-01 13:28:27 +0000

    propagate ClusterName to ec2 instances

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -93,7 +93,7 @@ resource "aws_autoscaling_group" "cluster" {
   tag {
     key                 = "Cluster"
     value               = "${var.cluster_name}"
-    propagate_at_launch = false
+    propagate_at_launch = true
   }
 }
 
