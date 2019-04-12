20b3ba19faac748b92b1bf7f4b32f20b2e1ab32d
Edward Viaene <ward@in4it.io>
2019-04-12 21:05:31 +0200

    README, IAM updates

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -116,3 +116,53 @@ resource "aws_iam_role_policy" "cluster-ec2-role" {
 }
 EOF
 }
+
+#
+# IAM Task execution role
+#
+resource "aws_iam_role" "ecs-task-execution-role" {
+  name = "ecs-${var.cluster_name}-task-execution-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs-task-execution-role" {
+  name = "ecs-task-execution-role"
+  role = "${aws_iam_role.ecs-task-execution-role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:BatchGetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "ssm:GetParameters",
+        "ssm:GetParameter"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
