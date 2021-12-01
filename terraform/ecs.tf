9d4a703bc63e33e6253abdaefa9e479b6734c14c
Alessandro Marino <alessandromarinoac@gmail.com>
2021-12-01 12:21:50 +0100

    Terraform changes

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -28,10 +28,10 @@ data "aws_ami" "ecs" {
 #
 
 resource "aws_ecs_cluster" "cluster" {
-  name = var.cluster_name
-  capacity_providers = var.ecs_capacity_provider_enabled ? [aws_ecs_capacity_provider.deploy[0].name] : []
+  name               = var.cluster_name
+  capacity_providers = var.ecs_capacity_provider_enabled ? [aws_ecs_capacity_provider.deploy_arm64[0].name,aws_ecs_capacity_provider.deploy_x86_64[0].name] : []
   dynamic "default_capacity_provider_strategy" {
-    for_each = aws_ecs_capacity_provider.deploy
+    for_each = var.ecs_capacity_provider_enabled ? [aws_ecs_capacity_provider.deploy_arm64[0],aws_ecs_capacity_provider.deploy_x86_64[0]] : []
     content {
       base              = 0
       capacity_provider = default_capacity_provider_strategy.value.name
@@ -42,7 +42,7 @@ resource "aws_ecs_cluster" "cluster" {
 
 data "template_file" "ecs_init" {
   template = file(
-    var.ecs_init_script == "" ? "${path.module}/templates/ecs-init.sh" : var.ecs_init_script,
+  var.ecs_init_script == "" ? "${path.module}/templates/ecs-init.sh" : var.ecs_init_script,
   )
 
   vars = {
@@ -66,12 +66,12 @@ resource "aws_launch_template" "cluster" {
   }
 
   vpc_security_group_ids = compact(
-    split(
-      ",",
-      format("%s,%s", aws_security_group.cluster.id, var.ecs_ec2_extra_sg),
-    ),
+  split(
+  ",",
+  format("%s,%s", aws_security_group.cluster.id, var.ecs_ec2_extra_sg),
+  ),
   )
-  user_data = base64encode(data.template_file.ecs_init.rendered)
+  user_data              = base64encode(data.template_file.ecs_init.rendered)
 
   credit_specification {
     cpu_credits = var.cpu_credits
@@ -102,14 +102,14 @@ resource "aws_launch_template" "cluster" {
 #
 # autoscaling
 #
-resource "aws_autoscaling_group" "cluster" {
-  name                 = "ecs-${var.cluster_name}-autoscaling"
-  vpc_zone_identifier  = var.vpc_private_subnets
-  termination_policies = var.cluster_termination_policies
-  min_size             = var.cluster_minsize
-  max_size             = var.cluster_maxsize
-  desired_capacity     = var.cluster_desired_capacity
-  protect_from_scale_in  = var.ecs_capacity_provider_enabled ? true : false
+resource "aws_autoscaling_group" "cluster_x86_64" {
+  name                  = "ecs-${var.cluster_name}-autoscaling-x86-64"
+  vpc_zone_identifier   = var.vpc_private_subnets
+  termination_policies  = var.cluster_termination_policies
+  min_size              = var.cluster_minsize
+  max_size              = var.cluster_maxsize
+  desired_capacity      = var.cluster_desired_capacity
+  protect_from_scale_in = var.ecs_capacity_provider_enabled ? true : false
 
   launch_template {
     id      = aws_launch_template.cluster.id
@@ -118,39 +118,95 @@ resource "aws_autoscaling_group" "cluster" {
 
   tag {
     key                 = "Name"
-    value               = "${var.cluster_name}-ecs"
+    value               = "${var.cluster_name}-ecs-x86-64"
     propagate_at_launch = true
   }
 
   tag {
     key                 = "Cluster"
-    value               = var.cluster_name
+    value               = "${var.cluster_name}_X86_64"
     propagate_at_launch = true
   }
 }
 
-resource "aws_autoscaling_lifecycle_hook" "cluster" {
-  count = var.ecs_capacity_provider_enabled ? 0 : 1
-  name                   = "${var.cluster_name}-hook"
-  autoscaling_group_name = aws_autoscaling_group.cluster.name
+resource "aws_autoscaling_group" "cluster_arm64" {
+  name                  = "ecs-${var.cluster_name}-autoscaling-arm64"
+  vpc_zone_identifier   = var.vpc_private_subnets
+  termination_policies  = var.cluster_termination_policies
+  min_size              = var.cluster_minsize
+  max_size              = var.cluster_maxsize
+  desired_capacity      = var.cluster_desired_capacity
+  protect_from_scale_in = var.ecs_capacity_provider_enabled ? true : false
+
+  launch_template {
+    id      = aws_launch_template.cluster.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.cluster_name}-ecs-arm64"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Cluster"
+    value               = "${var.cluster_name}_ARM64"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_lifecycle_hook" "cluster_x86_64" {
+  count                  = var.ecs_capacity_provider_enabled ? 0 : 1
+  name                   = "${var.cluster_name}-hook-x86-64"
+  autoscaling_group_name = aws_autoscaling_group.cluster_x86_64.name
   default_result         = "CONTINUE"
   heartbeat_timeout      = 900
   lifecycle_transition   = "autoscaling:EC2_INSTANCE_TERMINATING"
 }
 
-resource "aws_ecs_capacity_provider" "deploy" {
+
+resource "aws_autoscaling_lifecycle_hook" "cluster_arm64" {
+  count                  = var.ecs_capacity_provider_enabled ? 0 : 1
+  name                   = "${var.cluster_name}-hook-arm64"
+  autoscaling_group_name = aws_autoscaling_group.cluster_arm64.name
+  default_result         = "CONTINUE"
+  heartbeat_timeout      = 900
+  lifecycle_transition   = "autoscaling:EC2_INSTANCE_TERMINATING"
+}
+
+
+resource "aws_ecs_capacity_provider" "deploy_x86_64" {
   count = var.ecs_capacity_provider_enabled ? 1 : 0
-  name = "deploy"
+  name  = "deploy-x86-64"
 
   auto_scaling_group_provider {
-    auto_scaling_group_arn         = aws_autoscaling_group.cluster.arn
+    auto_scaling_group_arn         = aws_autoscaling_group.cluster_x86_64.arn
     managed_termination_protection = "ENABLED"
 
     managed_scaling {
-        maximum_scaling_step_size = var.capacity_maximum_scaling_step_size
-        minimum_scaling_step_size = var.capacity_minimum_scaling_step_size
-        status                    = "ENABLED"
-        target_capacity           = var.target_capacity
+      maximum_scaling_step_size = var.capacity_maximum_scaling_step_size
+      minimum_scaling_step_size = var.capacity_minimum_scaling_step_size
+      status                    = "ENABLED"
+      target_capacity           = var.target_capacity
     }
   }
 }
+
+resource "aws_ecs_capacity_provider" "deploy_arm64" {
+  count = var.ecs_capacity_provider_enabled ? 1 : 0
+  name  = "deploy-arm64"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.cluster_arm64.arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = var.capacity_maximum_scaling_step_size
+      minimum_scaling_step_size = var.capacity_minimum_scaling_step_size
+      status                    = "ENABLED"
+      target_capacity           = var.target_capacity
+    }
+  }
+}
+
