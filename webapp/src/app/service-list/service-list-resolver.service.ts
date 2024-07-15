3c921b63905f596a7d0cbdfbedd356e3a8abc794
Edward Viaene <ward@in4it.io>
2024-07-15 13:26:45 +0200

    angular update

--- webapp/src/app/service-list/service-list-resolver.service.ts
+++ webapp/src/app/service-list/service-list-resolver.service.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { AuthService } from '../services/auth.service';
 
